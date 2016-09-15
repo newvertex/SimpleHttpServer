0bba3e2c6c7c9144035a66924a798b57dacb4b71
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2016-09-15 15:23:58 +0430

    My Simple Http Server ;-)

---
+++ SimpleServer.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var contentType = {
+  '.html' : 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.xml': 'application/xml',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.png': 'image/png'
+};
+
+function getType(ext) {
+  return {'Content-Type': ext ? contentType[ext] : 'text/plain'};
+}
+
+function read(res, url) {
+  url = '.' + url;  //Go to current directory
+
+  if (url.length < 3) {  //If root requested then return index file
+    url += 'index.html';
+  }
+
+  fs.access(url, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.writeHead(404, getType());
+      res.end('404 Not Found.\n' + err);
+      return;
+    }
+
+    fs.readFile(url, (err, data) => {
+      if (err) {
+        res.writeHead(500, getType());
+        res.end('500 Internal error.\n</hr>Contact administrator.');
+        return;
+      }
+
+      res.writeHead(200, getType(path.extname(url)));
+      res.end(data);
+    });
+
+  });
+}
+
+http.createServer((req, res)=>{
+  var url = req.url;
+  read(res, url);
+}).listen(1337);
+console.log('Server Running....');
