5d0f88fab0f34acb7368a7a365b60da04027ccba
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2016-10-04 01:27:58 +0330

    Add command line args

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,11 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+let program = require('commander');
+const VERSION = require('./package.json').version;
+
+program
+  .version(VERSION)
+  .option('-p, --port <port-number>', 'Change server port')
+  .option('-l, --live-reload-url <url>', 'Change Live Reload url')
+  .parse(process.argv);
+
+const PORT = program.port || 1337;
 
 // LiveReload constants, Edit first line
-const liveReloadUrl = 'http://localhost:35729/livereload.js';
+const LIVE_RELOAD_URL = program.liveReloadUrl ||  'http://localhost:35729/livereload.js';
 
-const liveReloadTag = '<script type="text/javascript" src="' + liveReloadUrl + '"></script>';
+const LIVE_RELOAD_TAG = '<script type="text/javascript" src="' + LIVE_RELOAD_URL + '"></script>';
 
 if(!String.prototype.insertAt){
   String.prototype.insertAt = function(index = 0, string = '') {
@@ -55,7 +65,7 @@ function read(res, url) {
 	  // Add LiveReload script tag to all html page before close body tag
 	  if (path.extname(url) == '.html') {
 		data = String(data);
-        data = data.insertAt(data.lastIndexOf('</body>'), liveReloadTag);
+        data = data.insertAt(data.lastIndexOf('</body>'), LIVE_RELOAD_TAG);
       }
 
       res.writeHead(200, getType(path.extname(url)));
@@ -68,5 +78,5 @@ function read(res, url) {
 http.createServer((req, res)=>{
   var url = req.url;
   read(res, url);
-}).listen(1337);
-console.log('Server Running....');
+}).listen(PORT);
+console.log(`Server Running on ${PORT} ....`);
